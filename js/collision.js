433607453b5aa8c7ff1d1004e7aa2af355e80b16
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2024-01-09 14:52:58 -0700

    quick removal of a console.log()

--- js/collision.js
+++ js/collision.js
@@ -7,7 +7,6 @@ function collision() {
 function bulletDetection(n) {
     if (bullets[n].y + bullets[n].h < 0) {
         bullets.splice(n, 1);
-        console.log("border");
     } else {
         for (let i = 0; i < enemies.length; i++) {
             if (enemies[i].id === "discus" && bullets[n].x > enemies[i].x - enemies[i].r && bullets[n].x < enemies[i].x + enemies[i].r && bullets[n].y < enemies[i].y + enemies[i].r && bullets[n].y + bullets[n].h > enemies[i].y - enemies[i].r) {
